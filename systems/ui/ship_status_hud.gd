516cdb1c565e535ae127846e3b4101fed8f85eb3
serjoleinic2-cloud <serjoleinic2@gmail.com>
2026-09-10 11:23:15 +0300

    Add dock prompt and E action

--- systems/ui/ship_status_hud.gd
+++ systems/ui/ship_status_hud.gd
@@ -44,9 +44,16 @@ func _process(_delta: float) -> void:
 	var position: Vector2 = GameState.ship_state.get("position", Vector2.ZERO)
 	var discovered_count: int = GameState.player_state.get("discovered_port_ids", []).size()
 	var nearest_port: String = ""
+	var dock_candidate: String = ""
 
 	if _port_system != null:
 		nearest_port = _port_system.nearest_port_name
+		if _port_system.has_method("get_dock_candidate"):
+			dock_candidate = _port_system.get_dock_candidate()
+
+	var dock_hint: String = ""
+	if dock_candidate != "":
+		dock_hint = "\\nDock: press E"
 
 	_label.text = (
 		"SHIP STATUS\n"
@@ -57,7 +64,7 @@ func _process(_delta: float) -> void:
 		+ "Cargo: %d / %d\n"
 		+ "Ports known: %d\n"
 		+ "Nearest: %s\n"
-		+ "Pos: %d, %d"
+		+ "Pos: %d, %d"\n\t\t+ "%s"
 	) % [
 		int(money),
 		speed,
@@ -66,5 +73,15 @@ func _process(_delta: float) -> void:
 		cargo.size(), cargo_capacity,
 		discovered_count,
 		nearest_port if nearest_port != "" else "-",
-		int(position.x), int(position.y)
+		int(position.x), int(position.y), dock_hint
 	]
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if _port_system == null or not event is InputEventKey or not event.pressed or event.echo:
+		return
+	if event.keycode == KEY_E or event.physical_keycode == KEY_E:
+		var candidate: String = _port_system.get_dock_candidate()
+		if candidate != "":
+			_port_system.dock(candidate)
+			get_viewport().set_input_as_handled()
